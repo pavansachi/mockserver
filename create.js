21704c5019e3bc1df6b696c922b8a04915bcf425
pavansachi <pavan8sachi@gmail.com>
2017-02-09 08:01:45 +1100

    removed files

--- create.js
+++
@@ -1,5 +0,0 @@
-var mocks = require("./service.js");
-
-mocks.create("localhost", 8080);
-
-console.log("--------- mock expectations created ------");
--- reset.js
+++
@@ -1,5 +0,0 @@
-var mocks = require("./service.js");
-
-mocks.reset("localhost", 8080);
-
-console.log("--------- mock expectations reset ------");
